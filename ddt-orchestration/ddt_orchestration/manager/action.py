67af0e9efdf914a67f455a2b4a4c89914edf2f2a
ruichao <ruichao.wu@ipa.fraunhofer.de>
2022-02-04 18:38:38 +0100

    link page per application

---
+++ ddt-orchestration/ddt_orchestration/manager/action.py
@@ -0,0 +1,14 @@
+
+"""
+click -> notice probe to start zenoh bridge `./root/zenoh-bridge-dds -d $ROS_DOMAIN_ID -e tcp/zenoh-router-service:7447`
+->
+
+"""
+def show_ros_graph():
+    print("clever_function HI")
+
+def get_ros_graph():
+    pass
+
+def make_ros_graph():
+    gh_or = get_ros_graph()
