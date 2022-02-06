767de26954af07c9e72f546910ef6fd1b1d374ef
ruichao <ruichao.wu@ipa.fraunhofer.de>
2022-02-06 13:17:43 +0100

    add Process model

--- ddt-orchestration/ddt_orchestration/model/ddt_model.py
+++ ddt-orchestration/ddt_orchestration/model/ddt_model.py
@@ -1,3 +1,4 @@
+from fcntl import F_GETLEASE
 from .rosmodel import Node
 
 class Pod(object):
@@ -10,10 +11,28 @@ class Pod(object):
         if isinstance(node, Node):
             self.nodes.append(node)
 
+class Process(object):
+    def __init__(self, name):
+        self.name = name
+        self.state = False
+        self.pid = None
+    def set(self, **kwargs):
+        for k,v in kwargs.items():
+            if k in self.__dict__.keys():
+                setattr(self, k, v)
+    def start(self, process):
+        self.state = True
+        self.pid = process
+    def stop(self):
+        self.state = False
+        self.pid = None
+    def dict(self):
+        return dict((key, value) for key, value in self.__dict__.items() if not callable(value) and not key.startswith('__'))
 class Application(object):
     def __init__(self, name):
         self.name = name
         self.pods = list()
+        self.processes = list()
     def add_pod(self, pod):
         if isinstance(pod, Pod):
             self.pods.append(pod)
@@ -22,5 +41,15 @@ class Application(object):
             for node in pod.nodes:
                 if node.name == node_name:
                     return pod, node
-
-
+    def add_process(self, name):
+        p = Process(name)
+        self.processes.append(p)
+    def find_process(self, name):
+        for p in self.processes:
+            if p.name == name:
+                return p
+    def update_process(self, name, **kwargs):
+        p = self.find_process(name)
+        print(p.dict())
+        p.set(**kwargs)
+        print(p.dict())
