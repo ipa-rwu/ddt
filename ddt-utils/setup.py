9827cdc89944f55ef66f3f8184039a47ae3c5531
ruichao <ruichao.wu@ipa.fraunhofer.de>
2022-02-17 16:36:39 +0100

    add cmmon ddt model and utils in ddt-utils pkg

---
+++ ddt-utils/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="ddt-utils",
+    version="0.0.1",
+    author="Ruichao Wu",
+    license="Apache License, Version 2.0",
+    description="ddt probe",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        'pydantic',
+        'ros2-model',],
+    entry_points = {
+    },
+)
