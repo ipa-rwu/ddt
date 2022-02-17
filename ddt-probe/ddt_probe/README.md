080aa1ba15f753e125cf0fc470d3effa02771010
ruichao <ruichao.wu@ipa.fraunhofer.de>
2022-02-17 16:40:17 +0100

    move probe to separate pkg

---
+++ ddt-probe/ddt_probe/README.md
@@ -0,0 +1,6 @@
+#DDT Probe
+
+### start
+```
+ddt_probe "app_id" "probe_id" "probe ip" "domain id" "server_ip", "server_port"
+```
---
+++ ddt-probe/ddt_probe/utils/__init__.py
@@ -0,0 +1,6 @@
+class PodInfo:
+    def __init__(self, app_id, pod_id, pod_ip, domain_id):
+        self.app_id = app_id
+        self.pod_id = pod_id
+        self.pod_ip = pod_ip
+        self.domain_id = domain_id
