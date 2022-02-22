c644f6381a115e31ae624f5b1c63e13c29e896a8
ruichao <ruichao.wu@ipa.fraunhofer.de>
2022-02-22 19:48:33 +0100

    create docker imsges for each pkgs

--- ddt-probe/ddt_probe/run.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-ddt_probe chatter-galatic chatter-takler  127.0.0.1 42 127.0.0.1 1234
---
+++ ddt-probe/run.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+# ddt_probe chatter-galatic chatter-takler  127.0.0.1 42 127.0.0.1 1234
+ddt_probe ${APP_ID} ${POD_ID}  ${POD_IP} ${DOMAIN_ID} ${DDT_MANAGER_SERVER} ${DDT_MANAGER_PORT} &
+python3 -m http.server ${DDT_PROBE_SERVER_PORT} --bind ${DDT_PROBE_SERVER} --directory ${DDT_FOLDER}
